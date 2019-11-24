f28ee5e02e53faf3afaa09252c851c57c435b7b3
ghalib2015 <44357847+ghalib2015@users.noreply.github.com>
2019-11-24 14:10:30 +0100

    Add files via upload

---
+++ testing2.py
@@ -0,0 +1,24 @@
+import torch
+from classifier import SeqClassifier
+from dataset_seq import Balls_CF_Seq
+
+
+def load(name):
+    dataset = Balls_CF_Seq("mini_balls_seq", 699)
+    model = SeqClassifier()
+    model.cuda()
+    model.load_state_dict(torch.load(name + ".pth")["state_dict"])
+    model.eval()
+    model.train(False)
+    return model, dataset
+
+
+model, dataset = load("seqClassifier")
+valid = torch.utils.data.DataLoader(dataset,
+                                    batch_size=50, shuffle=True)
+x1, x2, x3, t = next(iter(valid))
+y = model(x1.cuda(), x2.cuda(), x3.cuda()).detach()
+for i in range(50):
+    print(y[i].view(3, 4))
+    print(t[i])
+    print("_______________________________________")
